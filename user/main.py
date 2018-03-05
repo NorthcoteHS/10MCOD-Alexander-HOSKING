602c622e219dd6c2aa52f77611c7fd22af6aac25
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-03-06 09:26:36 +1100

    HOSKING, Alexander: 2018-03-06 09:26:36

---
+++ user/main.py
@@ -0,0 +1,23 @@
+again= "yes"
+while again=="yes":
+
+ question= input ("Ask a question:")
+
+
+from random import randint
+number= (randint(0,4))
+
+
+if number == 1:
+   print ("Yes")
+if number ==2:
+  print ("No")
+if number == 3:
+  print ("Maybe")
+if number== 4:
+  print ("Ask again later")
+
+ again= input("Would you like to go again?")
+ 
+ while again== "No":
+   print("Goodbye")
